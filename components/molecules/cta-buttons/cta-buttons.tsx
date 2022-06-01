8fd7d261991a31cb2ba443b2080760bb4f9469ed
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-06-01 14:25:37 -0600

    adds CtaButtons

---
+++ components/molecules/cta-buttons/cta-buttons.tsx
@@ -0,0 +1,16 @@
+import { Box, Button } from "@obolnetwork/obol-ui";
+
+export const CtaButtons = () => {
+  return (
+    <Box css={{ display: "flex", flexWrap: "wrap", gap: "$sm" }}>
+      <Button
+        as="a"
+        target="_blank"
+        href="https://discord.com/invite/n6ebKsX46w"
+      >
+        Join the Community
+      </Button>
+      <Button color="secondary">Join the Team</Button>
+    </Box>
+  );
+};
