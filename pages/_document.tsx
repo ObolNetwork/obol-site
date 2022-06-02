4d859424a9b08880158f0058188aadb5d04cf811
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-06-01 18:36:41 -0600

    adds _document page

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from "@obolnetwork/obol-ui";
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style id="obol" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <script>0</script>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
