0548c86a8111b41b9058fbc3349a8e2c7e28204a
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2022-06-07 11:35:27 -0600

    adds top left background on page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import { globalCss } from "@obolnetwork/obol-ui";
 import type { AppProps } from "next/app";
-
+import TopLeftBg from '../public/backgrounds/top-left-bg.svg';
 const globalStyles = globalCss({
   html: {
     overflowX: "hidden",
   },
-
-  body: {
+  body: {    
+    background: `url(${TopLeftBg.src}) left top no-repeat`,
     backgroundColor: "$bg01",
     padding: 0,
     margin: 0,
