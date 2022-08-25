38ab9c791f0b12f419784f4760536e9fba06a6c8
f1lander <f1lander@users.noreply.github.com>
2022-08-25 19:13:20 +0000

    Deploy preview for PR 34 🛫

---
+++ pr-preview/pr-34/_next/static/T0tapF5SqVOt2UxRNAvs4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/139-0e5c5d0bba207dbe.js","static/chunks/pages/index-ce4d804b7be6404e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
