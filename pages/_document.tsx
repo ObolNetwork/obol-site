eca3a6a200a6edb10116683454e1496e896580a1
f1lander <filanderuclez@gmail.com>
2022-09-20 15:42:03 -0600

    adds script tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import React from "react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { getCssText } from "@obolnetwork/obol-ui";
@@ -7,14 +8,20 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          <style
-            id="obol"
+          <script
+            id="google-tag-manager"
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NZ2HGMB');${getCssText()}`,
+})(window,document,'script','dataLayer','GTM-NZ2HGMB');`,
+            }}
+          />
+          <style
+            id="obol"
+            dangerouslySetInnerHTML={{
+              __html: getCssText(),
             }}
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
