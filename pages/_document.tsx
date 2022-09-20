4b5a415e082e05afbfc45f2f5369308c14e966dc
f1lander <filanderuclez@gmail.com>
2022-09-20 15:22:24 -0600

    adds google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,16 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          <style id="obol" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+          <style
+            id="obol"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NZ2HGMB');${getCssText()}`,
+            }}
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
@@ -16,6 +25,12 @@ export default class Document extends NextDocument {
           />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZ2HGMB"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
+            }}
+          />
           <script>0</script>
           <Main />
           <NextScript />
