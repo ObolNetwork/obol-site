0bae7ad88a0b592a8b31e14c3bdcce75fa4f78d8
f1lander <filanderuclez@gmail.com>
2022-09-21 12:18:32 -0600

    change gtag id for testing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,7 @@ export default class Document extends NextDocument {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NZ2HGMB');`,
+})(window,document,'script','dataLayer','GTM-5DGSC5Z');`,
             }}
           />
           <style
@@ -34,7 +34,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <body>
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZ2HGMB"
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DGSC5Z"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           />
