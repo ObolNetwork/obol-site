33302494a208d717cf675d8f18a8729a2b58b31c
f1lander <f1lander@users.noreply.github.com>
2022-09-21 14:02:26 +0000

    Remove preview for PR 37 🛬

--- pr-preview/pr-37/_next/static/2fknGgyVXbpZ68poxyWDx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/139-0e5c5d0bba207dbe.js","static/chunks/pages/index-74d2e33837963e44.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
