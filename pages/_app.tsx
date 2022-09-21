403ab2e875ab898cde7e66ca5032e99f3be5daf1
eDAX <filanderuclez@gmail.com>
2022-09-21 12:44:47 -0600

    Ft/gtag manager (#38)
    * adds google tag manager
    
    * removes Nocscript tag
    
    * adds script tag
    
    * adds Script to app.tsx
    
    * move script to _document
    
    * remove script from app.tsx
    
    * change gtag id for testing
    
    * use gtm module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { globalCss } from "@obolnetwork/obol-ui";
 import type { AppProps } from "next/app";
-import Script from "next/script";
-import TopLeftBg from "../public/backgrounds/top-left-bg.svg";
+import TopLeftBg from '../public/backgrounds/top-left-bg.svg';
+
+import TagManager from 'react-gtm-module';
+import { useEffect } from "react";
+
 const globalStyles = globalCss({
   html: {
     overflowX: "hidden",
   },
-  body: {
+  body: {    
     background: `url(${TopLeftBg.src}) left top no-repeat`,
     backgroundColor: "$bg01",
     padding: 0,
@@ -21,20 +24,10 @@ const globalStyles = globalCss({
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
-  return (
-    <>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NZ2HGMB');
-      `}
-      </Script>
-      <Component {...pageProps} />;
-    </>
-  );
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-NZ2HGMB' });
+}, []);
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
