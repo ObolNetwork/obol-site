b02749b9db3187852a852c8604852ba69d319e3a
f1lander <filanderuclez@gmail.com>
2022-09-21 13:56:12 -0600

    fixes wrong char on script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 import { globalCss } from "@obolnetwork/obol-ui";
 import type { AppProps } from "next/app";
-import TopLeftBg from '../public/backgrounds/top-left-bg.svg';
-
-import TagManager from 'react-gtm-module';
-import { useEffect } from "react";
+import TopLeftBg from "../public/backgrounds/top-left-bg.svg";
 
 const globalStyles = globalCss({
   html: {
     overflowX: "hidden",
   },
-  body: {    
+  body: {
     background: `url(${TopLeftBg.src}) left top no-repeat`,
     backgroundColor: "$bg01",
     padding: 0,
@@ -24,9 +21,6 @@ const globalStyles = globalCss({
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
-  useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-NZ2HGMB' });
-}, []);
   return <Component {...pageProps} />;
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,16 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
+          <script
+            id="google-tag-manager"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NZ2HGMB');`,
+            }}
+          />
           <style
             id="obol"
             dangerouslySetInnerHTML={{
@@ -21,7 +31,14 @@ export default class Document extends NextDocument {
             rel="stylesheet"
           />
         </Head>
-        <body>         
+        <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZ2HGMB"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          <script>0</script>
           <Main />
           <NextScript />
         </body>
