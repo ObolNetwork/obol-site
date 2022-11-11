333e041d2773c35f3e9cec02dd02b8c2d3c92c8f
LukeHackett12 <LukeHackett12@users.noreply.github.com>
2022-11-11 09:24:44 +0000

    Deploy preview for PR 51 🛫

---
+++ pr-preview/pr-51/_next/static/dAOiXh7E6iwscScYyLw97/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/139-0e5c5d0bba207dbe.js","static/chunks/pages/index-be7b6cbc3219211f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
