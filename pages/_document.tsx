1e72f6be385930fdc1d3efb494b6c8801022544f
f1lander <filanderuclez@gmail.com>
2023-04-17 13:44:11 -0600

    adds font-display block

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/google-font-display */
 /* eslint-disable @next/next/google-font-preconnect */
 /* eslint-disable @next/next/no-sync-scripts */
 /* eslint-disable @next/next/next-script-for-ga */
@@ -37,7 +38,7 @@ export default class Document extends NextDocument {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500&display=optional"
+            href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500&display=block"
             rel="stylesheet"
           />
           <link
