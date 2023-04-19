1856d578a392f9b0e24183fef494201118c2537f
ethdax <filanderuclez@gmail.com>
2023-04-19 13:18:30 -0600

    upgrades to nextjs13 (#81)
    * upgrades to nextjs13
    
    * adds font-display block
    
    * removes unused disable rules
    
    * adds sync

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,20 @@
-/* eslint-disable @next/next/no-sync-scripts */
+/* eslint-disable @next/next/google-font-preconnect */
 /* eslint-disable @next/next/next-script-for-ga */
+/* eslint-disable @next/next/google-font-display */
 import React from "react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-import { getCssText } from "@obolnetwork/obol-ui";
 
+import { getCssText, styled } from "@obolnetwork/obol-ui";
+
+const Body = styled("body", {
+  fontFamily: "'DM Sans', sans-serif",
+});
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
         <Head>
-          <script
+          <script        
             id="google-tag-manager"
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -24,7 +29,8 @@ export default class Document extends NextDocument {
             src="https://app.termly.io/embed.min.js"
             data-auto-block="on"
             data-website-uuid="01eadf2f-d1f3-4db1-bdce-a3cb1cd9f4d7"
-          ></script>
+            async
+          />
           <style
             id="obol"
             dangerouslySetInnerHTML={{
@@ -34,14 +40,21 @@ export default class Document extends NextDocument {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500&display=block"
             rel="stylesheet"
           />
+          <link
+            rel="preload"
+            href="https://fonts.gstatic.com/s/dmsans/v13/TKwU8NIMJ0bKS1l1-.otf"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
         </Head>
-        <body>
+        <Body>
           <Main />
           <NextScript />
-        </body>
+        </Body>
       </Html>
     );
   }
