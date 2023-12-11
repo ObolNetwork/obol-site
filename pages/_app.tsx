73c8545a5f22a7e36f68905ab35657ecc5ae0d47
ethdax <filanderuclez@gmail.com>
2023-12-11 13:35:57 -0600

    adds blocked page (#95)
    * adds blocked page
    
    * removes unneeded script tag comments
    
    * removes cookies banner from blocked page
    
    * adds integrity attr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import { globalCss } from "@obolnetwork/obol-ui";
 import type { AppProps } from "next/app";
 import TopLeftBg from "../public/backgrounds/top-left-bg.svg";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const globalStyles = globalCss({
   html: {
     overflowX: "hidden",
   },
-  body: {    
+  body: {
     background: `url(${TopLeftBg.src}) left top no-repeat`,
     backgroundColor: "$bg01",
     padding: 0,
@@ -21,6 +23,28 @@ const globalStyles = globalCss({
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.pathname !== "/blocked") {
+      const script = document.createElement("script");
+      script.src = "https://app.termly.io/embed.min.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.setAttribute("data-auto-block", "on");
+      script.setAttribute(
+        "data-website-uuid",
+        "01eadf2f-d1f3-4db1-bdce-a3cb1cd9f4d7"
+      );
+
+      script.integrity =
+        "eb05b794d3ff3828122578afacb94693563557be9036d4dc882d70ce63d17a1c";
+      script.crossOrigin = "anonymous";
+
+      document.body.appendChild(script);
+    }
+  }, [router.pathname]);
+
   return <Component {...pageProps} />;
 }
 
---
+++ pages/blocked.tsx
@@ -0,0 +1,104 @@
+import type { NextPage } from "next";
+import { Button, Navbar, Box, Text } from "@obolnetwork/obol-ui";
+import Head from "next/head";
+
+const Blocked: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <style>{`
+          html, body, #__next {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            overflow: hidden; /* Prevents scrolling on the block page */
+          }
+          .navbar-wrapper a {
+            height: 48px;
+          }
+          .navbar-wrapper button {
+            display: none;
+          }
+        `}</style>
+        <title>Obol - Building Distributed Validators for Ethereum</title>
+        <meta
+          name="description"
+          content="The Obol Network is an ecosystem for trust minimized staking that allows people to create, test, run & co-ordinate distributed validators"
+        />
+        <meta name="title" content="Obol" />
+        <link rel="icon" href="/favicon.png" />
+        <meta property="og:url" content="https://obol.tech" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://obol.tech/assets/og-image-v2.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
+      <div className="navbar-wrapper">
+        <Navbar />
+      </div>
+      <Box
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "90%",
+        }}
+      >
+        <Box
+          css={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "78%",
+            width: "77%",
+            background: "url(/backgrounds/mundi.svg) no-repeat center center", // replace with your SVG path
+            backgroundSize: "cover",
+            "@md": {
+              width: "100%",
+            },
+          }}
+        >
+          <Text
+            as="h1"
+            css={{
+              color: "$textLight",
+              fontSize: "32px",
+              marginBottom: "16px",
+              px: "30%",
+              "@sm": {
+                px: "10%",
+              },
+              textAlign: "center",
+              lineHeight: "48px",
+            }}
+          >
+            This link is not currently available in certain geolocations
+          </Text>
+          <Text
+            css={{
+              fontSize: "18px",
+              color: "$body",
+              marginBottom: "32px",
+              "@sm": {
+                px: "10%",
+                textAlign: "center",
+              },
+            }}
+          >
+            You can still learn more about Obol by visiting our website
+          </Text>
+          <Button as="a" href="https://obol.tech" css={{ alignSelf: "center" }}>
+            Obol Website
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Blocked;
